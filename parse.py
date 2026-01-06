847ce0c9f8304977969c486e5c59e2daa5bbcc51
Mehdi Aouinati <maouinat@e1r10p1.1337.ma>
2026-01-06 18:22:48 +0100

    adding error handling

--- parse.py
+++ parse.py
@@ -1,3 +1,8 @@
+
+class SyntaxError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
 def parse(filename):
     with open(filename, "r") as file:
         infos = {}
@@ -10,36 +15,44 @@ def parse(filename):
             if not line or line.startswith("#"):
                 continue
 
-            line_result = line.split("=")
-            if len(line_result) != 2:
-                print("Error")
-                break
+            try:
+                line_result = line.split("=")
 
-            if line_result[1] == '':
-                print("error is empty")
-                break
+                if len(line_result) != 2:
+                    raise SyntaxError("missing key or value")
 
-            if line_result[0] in digit:
-                try:
-                    infos[line_result[0]] = int(line_result[1])
-                except ValueError:
-                    print("Error: the info is wrong")
-                    break
-            elif line_result[0] in coord:
-                try:
-                    coords= tuple(int(v.strip()) for v in line_result[1].split(','))
-                    infos[line_result[0]] = coords
-                except ValueError:
-                    print("Error: the info is wrong")
-                    break
-            elif line_result[0] == "PERFECT":
-                if line_result[1] == "True":
-                    infos[line_result[0]] = True
+                if line_result[1] == '':
+                    raise SyntaxError("the key is empty")
+
+                if line_result[0] in digit:
+                    try:
+                        infos[line_result[0]] = int(line_result[1])
+                    except ValueError:
+                        print("Error: wrong key type")
+                        return None
+
+                elif line_result[0] in coord:
+                    try:
+                        coords= tuple(int(v.strip()) for v in line_result[1].split(','))
+                        infos[line_result[0]] = coords
+                    except ValueError:
+                        print("Error: wrong key")
+                        return None
+
+                elif line_result[0] == "PERFECT":
+                    if line_result[1] == "True":
+                        infos[line_result[0]] = True
+                    else:
+                        infos[line_result[0]] = False
+
                 else:
-                    infos[line_result[0]] = False
-            else:
-                infos[line_result[0]] = line_result[1]
+                    infos[line_result[0]] = line_result[1]
+
+            except SyntaxError as e:
+                print(f"Error: {e}")
+                return None
+            
         return infos
-        # for n, info in infos.items():
-        #     print(f"key = {n} : value = {info}")
+        
+   
     
